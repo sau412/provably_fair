5d1c1a09e0a1b4f5f9232ec5d1c33bb81b054e89
Tsarev Vladimir <sau412@gmail.com>
2019-02-15 16:48:50 +0300

    Create language.php

---
+++ language.php
@@ -0,0 +1,627 @@
+<?php
+$json_language=<<<_END
+{
+"admin_change_settings_successfull":{
+        "en":"Global wallet settings changed",
+        "ru":"Глобальные настройки кошелька изменены",
+        "fr":"Paramètres généraux du portefeuille modifiés"
+        },
+"register_failed_password_mismatch":{
+        "en":"Register failed. Password mismatch.",
+        "ru":"Ошибка при регистрациии: пароли не совпадают.",
+        "fr":"Echec de l'inscription. Mots de passe non-concordant."
+        },
+"register_failed_invalid_password":{
+        "en":"Register failed. Invalid password.",
+        "ru":"Ошибка при регистрации: неправильный пароль.",
+        "fr":"Echec de l'inscription. Mot de passe invalide."
+        },
+"register_failed_invalid_login":{
+        "en":"Register failed. Invalid login.",
+        "ru":"Ошибка при регистрации: неправильный логин.",
+        "fr":"Echec de l'inscription. Login invalide."
+        },
+"register_failed_invalid_captcha":{
+        "en":"Register failed. Invalid captcha.",
+        "ru":"Ошибка при регистрации: неправильная каптча.",
+        "fr":"Echec de l'inscription. Captcha invalide."
+        },
+"register_failed_disabled":{
+        "en":"Register failed. Registration/login disabled.",
+        "ru":"Ошибка при регистрации: регистрация и вход на ресурсе отключены.",
+        "fr":"Echec de l'inscription. Inscription/Login désactivés."
+        },
+"register_successfull":{
+        "en":"Register successfull.",
+        "ru":"Регистрация прошла успешно.",
+        "fr":"Inscrpition réussie."
+        },
+"login_successfull":{
+        "en":"",
+        "ru":"",
+        "fr":""
+        },
+"login_failed_invalid_login":{
+        "en":"Login failed. Invalid login/password",
+        "ru":"Ошибка при входе: неправильный логин или пароль.",
+        "fr":"Echec du login. Login/mot de passe invalide."
+        },
+"login_failed_invalid_password":{
+        "en":"Login failed. Invalid login/password",
+        "ru":"Ошибка при входе: неправильный логин или пароль.",
+        "fr":"Echec du login. Login/mot de passe invalide."
+        },
+"login_failed_invalid_captcha":{
+        "en":"Login failed. Invalid captcha.",
+        "ru":"Ошибка при входе: неправильная каптча.",
+        "fr":"Echec du login. Captcha invalide."
+        },
+"login_failed_disabled":{
+        "en":"Login failed. Registration/login disabled.",
+        "ru":"Ошибка при входе: регистрация и вход на ресурсе отключены.",
+        "fr":"Echec du login. Inscription/login désactivés."
+        },
+"user_change_settings_failed_new_password_mismatch":{
+        "en":"Change settings failed. New password mismatch.",
+        "ru":"Ошибка при изменении настроек: пароли не совпадают.",
+        "fr":"Modification de paramètres échouée. Nouveau mot de passe non-concordant."
+        },
+"user_change_settings_failed_password_incorrect":{
+        "en":"Change settings failed. Password incorrect.",
+        "ru":"Ошибка при изменении настроек: неправильный пароль.",
+        "fr":"Modification de paramètres échouée. Mot de passe incorrect."
+        },
+"user_change_settings_successfull":{
+        "en":"Settings successfully changed",
+        "ru":"Настройки успешно изменены",
+        "fr":"Modification de paramètres réussie."
+        },
+"header_greeting":{
+        "en":"Welcome,",
+        "ru":"Вы вошли как",
+        "fr":"Bienvenu,"
+        },
+"logout_successfull":{
+        "en":"",
+        "ru":"",
+        "fr":""
+        },
+"alias_deleted":{
+        "en":"Address book entry deleted",
+        "ru":"Удалена запись из адресной книги",
+        "fr":"Entrée supprimée du répertoire d'adresse."
+        },
+"alias_changed":{
+        "en":"Address book entry changed",
+        "ru":"Запись в адресной книге изменена",
+        "fr":"Entrée du repertoire d'adresse modifiée"
+        },
+"send_failed":{
+        "en":"Sending failed",
+        "ru":"Ошибка при отправке",
+        "fr":"Echec de l'envoi"
+        },
+"send_successfull":{
+        "en":"Sending successfull",
+        "ru":"Успешно отправлено",
+        "fr":"Envoi réussi"
+        },
+"create_new_address_failed":{
+        "en":"Creating new address failed",
+        "ru":"Ошибка при создании нового адреса",
+        "fr":"Création de nouvelle adresse échouée"
+        },
+"create_new_address_successfull":{
+        "en":"New address requested",
+        "ru":"Новый адрес запрошен",
+        "fr":"Nouvelle adresse nécessaire"
+        },
+"footer_about":{
+        "en":"Opensource gridcoin online wallet (<a href='https://github.com/sau412/gridcoin_online_wallet'>github link</a>) by Vladimir Tsarev, my nickname is sau412 on telegram, twitter, facebook, gmail, github, vk.",
+        "ru":"Он-лайн кошелёк Gridcoin с открытым исходным кодом (<a href='https://github.com/sau412/gridcoin_online_wallet'>github</a>). Автор - Царев Владимир, мой ник sau412 в контакте, телеграме, твиттере, фейсбуке, gmail, github.",
+        "fr":"Portefeuille Gridcoin opensource en ligne (<a href='https://github.com/sau412/gridcoin_online_wallet'>github link</a>) par Vladimir Tsarev, mon pseudo est sau42 sur telegram, twitter, facebook, gmail, github, vk."
+        },
+"login_login":{
+        "en":"Login:",
+        "ru":"Логин:",
+        "fr":"Login:"
+        },
+"login_password":{
+        "en":"Password:",
+        "ru":"Пароль:",
+        "fr":"Mot de passe:"
+        },
+"login_submit":{
+        "en":"Login",
+        "ru":"Вход",
+        "fr":"Login"
+        },
+"new_address_submit":{
+        "en":"Request new receiving address",
+        "ru":"Запросить новый адрес",
+        "fr":"Demande de nouvelle adresse de réception"
+        },
+"register_login":{
+        "en":"Login:",
+        "ru":"Логин:",
+        "fr":"Login:"
+        },
+"register_mail":{
+        "en":"E-mail:",
+        "ru":"E-mail:",
+        "fr":"E-mail:"
+        },
+"register_password1":{
+        "en":"Password 1:",
+        "ru":"Пароль 1:",
+        "fr":"Mot de passe 1:"
+        },
+"register_password2":{
+        "en":"Password 2:",
+        "ru":"Пароль 2:",
+        "fr":"Mot de passe 2:"
+        },
+"register_withdraw":{
+        "en":"Withdraw address:",
+        "ru":"Адрес для вывода:",
+        "fr":"Adresse de retrait:"
+        },
+"register_submit":{
+        "en":"Register",
+        "ru":"Регистрация",
+        "fr":"Inscription"
+        },
+"tab_info":{
+        "en":"Info",
+        "ru":"Информация",
+        "fr":"Information"
+        },
+"tab_dashboard":{
+        "en":"Dashboard",
+        "ru":"Основное",
+        "fr":"Tableau de bord"
+        },
+"tab_send":{
+        "en":"Send",
+        "ru":"Отправка",
+        "fr":"Envoyer"
+        },
+"tab_receive":{
+        "en":"Receive",
+        "ru":"Получение",
+        "fr":"Recevoir"
+        },
+"tab_transactions":{
+        "en":"Transactions",
+        "ru":"Транзкции",
+        "fr":"Transactions"
+        },
+"tab_address_book":{
+        "en":"Address book",
+        "ru":"Адресная книга",
+        "fr":"Répertoire d'adresses"
+        },
+"tab_settings":{
+        "en":"Settings",
+        "ru":"Настройки",
+        "fr":"Paramètres"
+        },
+"tab_control":{
+        "en":"Control",
+        "ru":"Управление",
+        "fr":"Contrôle"
+        },
+"tab_log":{
+        "en":"Log",
+        "ru":"Лог",
+        "fr":"Log"
+        },
+"tab_login":{
+        "en":"Login",
+        "ru":"Вход",
+        "fr":"Login"
+        },
+"tab_register":{
+        "en":"Register",
+        "ru":"Регистрация",
+        "fr":"Inscription"
+        },
+"dashboard_balance":{
+        "en":"Balance:",
+        "ru":"Баланс:",
+        "fr":"Solde:"
+        },
+"send_address":{
+        "en":"Address:",
+        "ru":"Адрес:",
+        "fr":"Adresse:"
+        },
+"send_amount":{
+        "en":"Amount:",
+        "ru":"Количество:",
+        "fr":"Montant:"
+        },
+"send_submit":{
+        "en":"Send",
+        "ru":"Отправить",
+        "fr":"Envoyer"
+        },
+"address_book_header":{
+        "en":"Address book",
+        "ru":"Адресная книга",
+        "fr":"Répertoire d'adresse"
+        },
+"address_book_label":{
+        "en":"Label:",
+        "ru":"Название:",
+        "fr":"Note:"
+        },
+"address_book_address":{
+        "en":"address:",
+        "ru":"адрес:",
+        "fr":"adresse:"
+        },
+"address_book_submit":{
+        "en":"Add",
+        "ru":"Добавить",
+        "fr":"Ajouter"
+        },
+"address_book_table_header_label":{
+        "en":"Label",
+        "ru":"Название",
+        "fr":"Note"
+        },
+"address_book_table_header_address":{
+        "en":"Address",
+        "ru":"Адрес",
+        "fr":"Adresse"
+        },
+"receive_header":{
+        "en":"Receiving addresses",
+        "ru":"Адреса для получения",
+        "fr":"Adresses de réception"
+        },
+"receive_generating":{
+        "en":"generating...",
+        "ru":"генерация...",
+        "fr":"en cours de génération..."
+        },
+"receive_table_header_address":{
+        "en":"Address",
+        "ru":"Адрес",
+        "fr":"Adresse"
+        },
+"receive_table_header_received":{
+        "en":"Received,",
+        "ru":"Получено,",
+        "fr":"Reçu,"
+        },
+"transactions_header":{
+        "en":"Transactions",
+        "ru":"Транзакции",
+        "fr":"Transactions"
+        },
+"transactions_table_header_address":{
+        "en":"Address",
+        "ru":"Адрес",
+        "fr":"Adresse"
+        },
+"transactions_table_header_amount":{
+        "en":"Amount,",
+        "ru":"Количество,",
+        "fr":"Montant,"
+        },
+"transactions_table_header_status":{
+        "en":"Status",
+        "ru":"Состояние",
+        "fr":"Statut"
+        },
+"transactions_table_header_tx_id":{
+        "en":"TX ID",
+        "ru":"TX ID",
+        "fr":"TX_ID"
+        },
+"transactions_table_header_timestamp":{
+        "en":"Timestamp",
+        "ru":"Время",
+        "fr":"Horodatage"
+        },
+"transactions_table_status_sent":{
+        "en":"sent",
+        "ru":"отправлено",
+        "fr":"envoyé"
+        },
+"transactions_table_status_received":{
+        "en":"received",
+        "ru":"получено",
+        "fr":"reçu"
+        },
+"transactions_table_status_processing":{
+        "en":"processing",
+        "ru":"обработка",
+        "fr":"processus en cours"
+        },
+"transactions_table_status_pending":{
+        "en":"pending",
+        "ru":"подтверждается",
+        "fr":"en attente"
+        },
+"transactions_table_status_sending_error":{
+        "en":"sending error",
+        "ru":"ошибка при отправке",
+        "fr":"erreur à l'envoi"
+        },
+"transactions_table_status_address_error":{
+        "en":"address error",
+        "ru":"ошибка в адресе",
+        "fr":"erreur d'adresse"
+        },
+"client_state_header":{
+        "en":"Client state:",
+        "ru":"Состояние клиента:",
+        "fr":"Statut du client:"
+        },
+"client_state_current_block":{
+        "en":"Current block:",
+        "ru":"Текущий блок:",
+        "fr":"Bloc en cours:"
+        },
+"client_state_current_block_hash":{
+        "en":"Hash:",
+        "ru":"Хэш:",
+        "fr":"Hash:"
+        },
+"client_state_payouts":{
+        "en":"Payouts:",
+        "ru":"Выплаты:",
+        "fr":"Paiements:"
+        },
+"client_state_api":{
+        "en":"API:",
+        "ru":"API:",
+        "fr":"API"
+        },
+"client_state_client_state":{
+        "en":"Client state:",
+        "ru":"Состояние клиента:",
+        "fr":"Statut du client:"
+        },
+"client_state_enabled":{
+        "en":"enabled",
+        "ru":"разрешено",
+        "fr":"activé"
+        },
+"client_state_disabled":{
+        "en":"disabled",
+        "ru":"запрещено",
+        "fr":"désactivé"
+        },
+"client_state_on":{
+        "en":"on",
+        "ru":"включено",
+        "fr":"actif"
+        },
+"client_state_off":{
+        "en":"off",
+        "ru":"выключено",
+        "fr":"inactif"
+        },
+"client_state_hour":{
+        "en":"h",
+        "ru":"ч",
+        "fr":"h"
+        },
+"client_state_minute":{
+        "en":"min",
+        "ru":"мин",
+        "fr":"min"
+        },
+"settings_header":{
+        "en":"Settings",
+        "ru":"Настройки",
+        "fr":"Paramètres"
+        },
+"settings_mail":{
+        "en":"E-mail:",
+        "ru":"E-mail:",
+        "fr":"E-mail:"
+        },
+"settings_notifications":{
+        "en":"notifications state",
+        "ru":"состояние оповещений",
+        "fr":"Statut des notifications"
+        },
+"settings_enabled":{
+        "en":"enabled",
+        "ru":"включено",
+        "fr":"activé"
+        },
+"settings_disabled":{
+        "en":"disabled",
+        "ru":"выключено",
+        "fr":"désactivé"
+        },
+"settings_api_state":{
+        "en":"API state:",
+        "ru":"состояние API:",
+        "fr":"statut API:"
+        },
+"settings_yes":{
+        "en":"yes",
+        "ru":"да",
+        "fr":"oui"
+        },
+"settings_no":{
+        "en":"no",
+        "ru":"нет",
+        "fr":"non"
+        },
+"settings_regenerate_api_key":{
+        "en":"regenerate API key",
+        "ru":"пересоздать ключ API",
+        "fr":"regénérer la clé API"
+        },
+"settings_api_key":{
+        "en":"API key:",
+        "ru":"ключ API:",
+        "fr":"clé API:"
+        },
+"settings_withdraw_address":{
+        "en":"Withdraw address:",
+        "ru":"Адрес для вывода:",
+        "fr":"Adresse de retrait:"
+        },
+"settings_password":{
+        "en":"Password (required to change settings):",
+        "ru":"Пароль (необходим для смены настроек):",
+        "fr":"Mot de passe (nécessaire pour modifier les paramètres):"
+        },
+"settings_new_password1":{
+        "en":"New password (if you want to change password):",
+        "ru":"Новы пароль (если вы хотите сменить ваш пароль):",
+        "fr":"Nouveau mot de passe (si vous voulez changer de mot de passe):"
+        },
+"settings_new_password2":{
+        "en":"New password one more time:",
+        "ru":"Новый пароль ещё раз:",
+        "fr":"Nouveau mot de passe à nouveau:"
+        },
+"settings_submit":{
+        "en":"Apply",
+        "ru":"Применить",
+        "fr":"Appliquer"
+        },
+"log_header":{
+        "en":"Log",
+        "ru":"Лог",
+        "fr":"Log"
+        },
+"log_table_header_timestamp":{
+        "en":"Timestamp",
+        "ru":"Время",
+        "fr":"Horodatage"
+        },
+"log_table_header_message":{
+        "en":"Message",
+        "ru":"Сообщение",
+        "fr":"Message"
+        },
+"info_header":{
+        "en":"Info",
+        "ru":"Информация",
+        "fr":"Information"
+        },
+"loading_block":{
+        "en":"Loading block...",
+        "ru":"Загрузка блока...",
+        "fr":"Chargement des blocs..."
+        },
+"link_block_explorer":{
+        "en":"block explorer",
+        "ru":"обозреватель блоков",
+        "fr":"explorateur de blocs"
+        },
+"link_send_to":{
+        "en":"send to",
+        "ru":"отправить",
+        "fr":"envoyer à"
+        },
+"link_address_book":{
+        "en":"address book",
+        "ru":"Адресная книга",
+        "fr":"repertoire d'adresse"
+        },
+"wallet_settings_header":{
+        "en":"Wallet settings",
+        "ru":"Настройки кошелька",
+        "fr":"Paramètres du portefeuille"
+        },
+"wallet_settings_enabled":{
+        "en":"enabled",
+        "ru":"включено",
+        "fr":"activé"
+        },
+"wallet_settings_disabled":{
+        "en":"disabled",
+        "ru":"выключено",
+        "fr":"désactivé"
+        },
+"wallet_settings_login_state":{
+        "en":"Login/register state:",
+        "ru":"Состояние входа и регистрации:",
+        "fr":"Statut du login/mot de passe:"
+        },
+"wallet_settings_payouts_state":{
+        "en":"payouts state:",
+        "ru":"состояние выплат:",
+        "fr":"statut des paiements:"
+        },
+"wallet_settings_api_state":{
+        "en":"API state:",
+        "ru":"состояние API:",
+        "fr":"statut de l'API:"
+        },
+"wallet_settings_global_message":{
+        "en":"Global message:",
+        "ru":"Глобальное сообщение:",
+        "fr":"Message global:"
+        },
+"wallet_settings_info":{
+        "en":"Info block:",
+        "ru":"Информационный блок:",
+        "fr":"Info bloc:"
+        },
+"wallet_settings_submit":{
+        "en":"Apply",
+        "ru":"Применить",
+        "fr":"Appliquer"
+        }
+}
+_END;
+
+function lang_load($lang_code) {
+        global $json_language;
+        global $default_language;
+        if($lang_code=='') $lang_code=$default_language;
+        $json_language_parsed=json_decode($json_language);
+        $current_language=array();
+        foreach($json_language_parsed as $key=>$lang_variable) {
+                if(property_exists($lang_variable,$lang_code)) $current_language[$key]=$lang_variable->$lang_code;
+        }
+        return $current_language;
+}
+
+function lang_message($code) {
+        global $current_language;
+        if(isset($current_language[$code])) {
+                return $current_language[$code];
+        }
+        return $code;
+}
+
+function lang_select_form($token) {
+        return <<<_END
+<form name=change_language method=post class='lang_selector'>
+<input type=hidden name=action value='change_lang'>
+<input type=hidden name=token value='$token'>
+<select name=lang onChange='form.submit();'>
+<option>language</option>
+<option value='en'>English</option>
+<option value='ru'>Русский</option>
+<option value='fr'>En français</option>
+</select>
+</form>
+_END;
+}
+
+function lang_parser($text) {
+        while(preg_match('/%([A-Za-z0-9_]+)%/',$text,$matches)) {
+                $replace_from=$matches[0];
+                $replace_to=lang_message($matches[1]);
+//echo "Replacing from '$replace_from' to '$replace_to'<br>";
+                $text=str_replace($replace_from,$replace_to,$text);
+        }
+        return $text;
+}
+
+?>
