a7815c9846add91986161b015d24c9167bbff821
Tsarev Vladimir <sau412@gmail.com>
2019-02-15 16:50:18 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,48 @@
+<?php
+// Settings file
+
+$wallet_name="Gridcoin Dice";
+$wallet_domain="dice.arikado.ru";
+$wallet_receive_confirmations=10;
+$currency_short="GRC";
+$currency="Gridcoin";
+
+// DB variables
+$db_host="";
+$db_login="";
+$db_password="";
+$db_base="";
+
+// Salt for password
+$salt="";
+
+// Email service
+$email_api_url="https://api.smtp2go.com/v3/email/send";
+$email_api_key="";
+$email_sender="";
+$email_reply_to="";
+$email_notify="";
+
+// ReCAPTHCA
+$recaptcha_private_key="";
+$recaptcha_public_key="";
+
+// Site settings
+$settings_api_enabled=TRUE;
+$settings_payouts_enabled=TRUE;
+$settings_login_enabled=TRUE;
+$default_language="en";
+
+// Gridcoin URLs
+$address_url="https://www.gridcoinstats.eu/address/";
+$tx_url="https://www.gridcoinstats.eu/tx/";
+$block_url="https://www.gridcoinstats.eu/block/";
+
+// Cooldown
+$free_roll_cooldown_interval=300;
+
+// Gridcoin online wallet API key
+$grc_api_url="https://wallet.arikado.ru/api.php";
+$grc_api_key="";
+
+?>
