e12c24f4ad6e1c0d850b6d710ef9b3d2e9d0159e
sau412 <sau412@protonmail.com>
2019-10-18 10:19:09 +0300

    More stats

--- web/cacti_stats.php
+++ web/cacti_stats.php
@@ -7,5 +7,8 @@ db_connect();
 $wallet_balance=db_query_to_variable("SELECT `value` FROM `variables` WHERE `name`='wallet_balance'");
 $users_balance=db_query_to_variable("SELECT SUM(`balance`) FROM `users`");
 $free_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type`='free'");
-echo "wallet_balance:$wallet_balance users_balance:$users_balance free_rolls:$free_rolls\n";
+$bet_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type` IN ('high','low')");
+$pay_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type` IN ('pay')");
+
+echo "wallet_balance:$wallet_balance users_balance:$users_balance free_rolls:$free_rolls bet_rolls:$bet_rolls pay_rolls:$pay_roll\n";
 ?>
