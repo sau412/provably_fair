445158903f329b3912d8f8f3bd8e05ef049a30a7
sau412 <sau412@protonmail.com>
2019-10-28 10:27:01 +0300

    Lottery stats

--- web/cacti_stats.php
+++ web/cacti_stats.php
@@ -9,14 +9,27 @@ $users_balance=db_query_to_variable("SELECT SUM(`balance`) FROM `users`");
 $free_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type`='free'");
 $bet_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type` IN ('high','low')");
 $pay_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type` IN ('pay')");
+$lottery_tickets=db_query_to_variable("SELECT SUM(`tickets`)
+	FROM `lottery_tickets`
+	WHERE `round_uid` IN (
+		SELECT `uid` FROM `lottery_rounds`
+		WHERE `stop` IS NULL)");
+$lottery_funds=db_query_to_variable("SELECT SUM(`spent`)
+	FROM `lottery_tickets`
+	WHERE `round_uid` IN (
+		SELECT `uid` FROM `lottery_rounds`
+		WHERE `stop` IS NULL)");
 $total_users=db_query_to_variable("SELECT count(*) FROM `users`");
-$active_users=db_query_to_variable("SELECT count(DISTINCT `user_uid`) FROM `rolls` WHERE DATE_SUB(NOW(),INTERVAL 1 DAY)<`timestamp`");
+$active_users=db_query_to_variable("SELECT count(DISTINCT `user_uid`)
+	FROM `rolls` WHERE DATE_SUB(NOW(),INTERVAL 1 DAY)<`timestamp`");
 
 echo "total_users:$total_users";
 echo " active_users:$active_users";
 echo " wallet_balance:$wallet_balance";
 echo " users_balance:$users_balance";
 echo " free_rolls:$free_rolls";
+echo " lottery_tickets:$lottery_tickets";
+echo " lottery_funds:$lottery_funds";
 echo " bet_rolls:$bet_rolls";
 echo " pay_rolls:$pay_rolls";
 ?>
