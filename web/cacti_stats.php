d949d5d45ad626d779c4264bf8e669506d30151e
sau412 <sau412@protonmail.com>
2020-05-19 09:57:10 +0300

    One query instead of two

--- web/cacti_stats.php
+++ web/cacti_stats.php
@@ -8,31 +8,23 @@ $wallet_balance=db_query_to_variable("SELECT `value` FROM `variables` WHERE `nam
 $users_balance=db_query_to_variable("SELECT SUM(`balance`) FROM `users`");
 
 $rolls_stats=db_query_to_array("SELECT
-SUM(IF(`roll_type` IN ('free'),1,0)) AS 'free',
-SUM(IF(`roll_type` IN ('high','low'),1,0)) AS 'bet',
-SUM(IF(`roll_type` IN ('pay'),1,0)) AS 'pay'
-FROM `rolls`");
+		SUM(IF(`roll_type` IN ('free'),1,0)) AS 'free',
+		SUM(IF(`roll_type` IN ('high','low'),1,0)) AS 'bet',
+		SUM(IF(`roll_type` IN ('pay'),1,0)) AS 'pay'
+	FROM `rolls`");
 
 $free_rolls = $rolls_stats[0]['free'];
 $bet_rolls = $rolls_stats[0]['bet'];
 $pay_rolls = $rolls_stats[0]['pay'];
 
-/*
-$free_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type`='free'");
-$bet_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type` IN ('high','low')");
-$pay_rolls=db_query_to_variable("SELECT count(*) FROM `rolls` WHERE `roll_type` IN ('pay')");
-*/
+$lottery_stats = db_query_to_array("SELECT SUM(`spent`) AS spent, SUM(`tickets`) AS tickets
+	FROM `lottery_tickets`
+    JOIN `lottery_rounds` ON `lottery_rounds`.`uid` = `lottery_tickets`.`round_uid`
+	WHERE `lottery_rounds`.`stop` IS NULL");
 
-$lottery_tickets=db_query_to_variable("SELECT SUM(`tickets`)
-	FROM `lottery_tickets`
-	WHERE `round_uid` IN (
-		SELECT `uid` FROM `lottery_rounds`
-		WHERE `stop` IS NULL)");
-$lottery_funds=db_query_to_variable("SELECT SUM(`spent`)
-	FROM `lottery_tickets`
-	WHERE `round_uid` IN (
-		SELECT `uid` FROM `lottery_rounds`
-		WHERE `stop` IS NULL)");
+$lottery_tickets = $lottery_stats[0]['tickets'];
+$lottery_funds = $lottery_stats[0]['spent'];
+
 $total_users=db_query_to_variable("SELECT count(*) FROM `users`");
 $active_users=db_query_to_variable("SELECT count(DISTINCT `user_uid`)
 	FROM `rolls` WHERE DATE_SUB(NOW(),INTERVAL 1 DAY)<`timestamp`");
