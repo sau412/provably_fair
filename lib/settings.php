dffd9373b7a50906d387b9f2b0804c4603c4d66e
sau412 <sau412@protonmail.com>
2020-06-27 14:37:41 +0300

    Settings updated

--- lib/settings.php
+++ lib/settings.php
@@ -1,11 +1,17 @@
 <?php
 // Settings file
 
-$wallet_name="Gridcoin Dice";
-$wallet_domain="dice.arikado.ru";
+$wallet_name="";
+$wallet_domain="";
 $wallet_receive_confirmations=10;
-$currency_short="GRC";
-$currency="Gridcoin";
+$currency_short="";
+$currency="";
+// For counter
+$project_counter_name="";
+
+// For logging
+$project_log_name="";
+$logger_url = "";
 
 // DB variables
 $db_host="";
@@ -16,27 +22,7 @@ $db_base="";
 // Salt for password
 $salt="";
 
-// Email service
-$email_api_url="https://api.smtp2go.com/v3/email/send";
-$email_api_key="";
-$email_sender="";
-$email_reply_to="";
-$email_notify="";
-
-// ReCAPTHCA
-$recaptcha_private_key="";
-$recaptcha_public_key="";
-
-// Daily percentage
-$daily_percentage=0;
-
-// Lottery
-$lottery_ticket_price=0.001;
-$lottery_round_length=86400;
-$lottery_min_funds=10;
-
 // Site settings
-$settings_api_enabled=TRUE;
 $settings_payouts_enabled=TRUE;
 $settings_login_enabled=TRUE;
 $default_language="en";
@@ -47,10 +33,26 @@ $tx_url="https://www.gridcoinstats.eu/tx/";
 $block_url="https://www.gridcoinstats.eu/block/";
 
 // Cooldown
-$free_roll_cooldown_interval=300;
+$free_roll_cooldown_interval=3600;
+
+// Daily percentage
+$daily_percentage=0.0134;
+
+// Lottery
+$lottery_ticket_price=0.001;
+$lottery_round_length=86400;
+$lottery_min_funds=10;
+
+// Recaptcha
+$recaptcha_public_key="";
+$recaptcha_private_key="";
 
 // Gridcoin online wallet API key
 $grc_api_url="https://wallet.arikado.ru/api.php";
 $grc_api_key="";
 
+// Broker settings
+$broker_url = "";
+$broker_key = "";
+$broker_project_name = "";
 ?>
