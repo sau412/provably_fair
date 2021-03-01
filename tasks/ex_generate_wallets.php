6f7598498a4d939b32394a9f4aa9a56af9ce6630
sau412 <sau412@protonmail.com>
2021-03-01 21:22:22 +0300

    Generate wallet addresses script

---
+++ tasks/ex_generate_wallets.php
@@ -0,0 +1,47 @@
+<?php
+
+// Update currency rates
+require_once("../lib/settings.php");
+require_once("../lib/db.php");
+require_once("../lib/core.php");
+require_once("../lib/broker.php");
+require_once("../lib/logger.php");
+require_once("../lib/gridcoin_web_wallet.php");
+
+db_connect();
+
+$wallets_data = db_query_to_array("SELECT w.`uid`, w.`currency_uid`, c.`symbol`,
+                                        w.`wallet_uid`, c.`wallet_api`, c.`wallet_key`,
+                                        c.`deposit_address`
+                                    FROM `ex_wallets` AS w
+                                    JOIN `ex_currencies` AS c ON w.`currency_uid` = c.`uid`");
+
+foreach($wallets_data as $wallet_row) {
+    $uid = $wallet_row['uid'];
+    $wallet_uid = $wallet_row['wallet_uid'];
+    $deposit_address = $wallet_row['deposit_address'];
+
+    $grc_api_url = $wallet_row['wallet_api'];
+    $grc_api_key = $wallet_row['wallet_key'];
+
+    $uid_escaped = db_escape($uid);
+    
+    if($wallet_uid) {
+        if(!$deposit_address) {
+            $deposit_address = grc_web_get_receiving_address($wallet_uid);
+            $deposit_address_escaped = db_escape($deposit_address);
+            db_query("UPDATE `ex_wallets`
+                        SET `deposit_address` = '$deposit_address_escaped'
+                        WHERE `uid` = '$uid_escaped'");
+        }
+    }
+    else {
+        $wallet_uid = grc_web_get_new_receiving_address();
+        if($wallet_uid) {
+            $wallet_uid_escaped = db_escape($wallet_uid);
+            db_query("UPDATE `ex_wallets`
+                        SET `wallet_uid` = '$wallet_uid_escaped'
+                        WHERE `uid` = '$uid_escaped'");
+        }
+    }
+}
