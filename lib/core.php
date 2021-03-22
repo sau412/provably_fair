ecd00b284e258998218c497dd621715153fd32e1
sau412 <sau412@protonmail.com>
2021-03-22 21:54:11 +0300

    Lock fix

--- lib/core.php
+++ lib/core.php
@@ -372,7 +372,8 @@ function user_withdraw($user_uid, $amount) {
         if($address=="") return FALSE;
 
 	// Lock tables
-	db_query("LOCK TABLES `users` WRITE, `transactions` WRITE, `rolls` WRITE, `minesweeper` WRITE, `lottery_tickets` WRITE");
+	db_query("LOCK TABLES `users` WRITE, `transactions` WRITE, `rolls` WRITE, `minesweeper` WRITE, `lottery_tickets` WRITE,
+                `ex_transactions` READ, `ex_exchanges` READ");
 
         // Check user balance
         $balance=get_user_balance($user_uid);
