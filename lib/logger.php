e7d220768d3f94acdaf6d82f391b8d7e52d8b4ad
sau412 <sau@arikado.xyz>
2023-05-12 14:03:47 +0300

    Log to syslog

--- lib/logger.php
+++ lib/logger.php
@@ -4,17 +4,7 @@ function log_write($message, $severity = 7) {
         global $logger_url;
         global $project_log_name;
 
-        $ch = curl_init($logger_url);
-        $body = json_encode([
-        	"source" => $project_log_name,
-        	"severity" => $severity,
-        	"message" => $message,
-        ]);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-        $result_json = curl_exec($ch);
-        curl_close($ch);        
-        $result = json_decode($result_json, true);
-        return $result;
+        openlog('php', LOG_ODELAY, LOG_USER);
+	syslog($severity, $message);
+	closelog();
 }
