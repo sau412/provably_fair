d04fd61ab4aeda45c9a95d5023d5504f110d49d2
sau412 <sau@arikado.xyz>
2023-05-12 14:06:51 +0300

    Encode non-stings into json

--- lib/logger.php
+++ lib/logger.php
@@ -1,10 +1,16 @@
 <?php
 
-function log_write($message, $severity = 7) {
+function log_write($messageRaw, $severity = 7) {
         global $logger_url;
         global $project_log_name;
 
         openlog('php', LOG_ODELAY, LOG_USER);
+        if(gettype($message) !== "string") {
+                $message = json_encode($messageRaw);
+        }
+        else {
+                $message = $messageRaw;
+        }
 	syslog($severity, $project_log_name . " " . $message);
 	closelog();
 }
