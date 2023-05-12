35ba3d01c780ae53289ac3f08e0eb6ff2eaf174a
sau412 <sau@arikado.xyz>
2023-05-12 14:20:42 +0300

    Variable name fix

--- lib/logger.php
+++ lib/logger.php
@@ -5,7 +5,7 @@ function log_write($messageRaw, $severity = 7) {
         global $project_log_name;
 
         openlog('php', LOG_ODELAY, LOG_USER);
-        if(gettype($message) !== "string") {
+        if(gettype($messageRaw) !== "string") {
                 $message = json_encode($messageRaw);
         }
         else {
