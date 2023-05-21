f00bd9f70b6e76b4a551e76ae7a9edc6bb709c74
sau412 <sau@arikado.xyz>
2023-05-21 18:38:38 +0300

    Cleanup rolls test

--- tasks/cleanup_rolls.php
+++ tasks/cleanup_rolls.php
@@ -10,25 +10,37 @@ require_once("../lib/logger.php");
 db_connect();
 
 $user_uids_array = db_query_to_array("
-    SELECT `user_uid`, SUM(`profit`) as total_profit
-    FROM `minesweeper`
-    GROUP BY `user_uid`
+SELECT `user_uid`, SUM(`bet`) as total_bet, SUM(`profit`) as total_profit,
+        YEAR(`timestamp`) as y, MONTH(`timestamp`) as m
+    FROM `rolls`
+    WHERE `user_uid` = 1
+        `timestamp` < DATE_SUB(CURRENT_DATE, INTERVAL 3 MONTH)
+    GROUP BY `user_uid`, YEAR(`timestamp`), MONTH(`timestamp`)
 ");
 
 foreach($user_uids_array as $row) {
     $user_uid = $row['user_uid'];
+    $total_bet = $row['total_bet'];
     $total_profit = $row['total_profit'];
-    if($total_profit <= 0) continue;
-    echo "Cleanup for user uid $user_uid\n";
+    $total_result = $total_profit - $total_bet;
+    $year = $row['y'];
+    $month = $row['m'];
+    
+    echo "Cleanup for user uid $user_uid year $year month $month\n";
     db_query("START TRANSACTION");
     db_query("
         INSERT INTO `rolls` (`user_uid`, `roll_type`, `server_seed`,
             `user_seed`, `roll_result`, `bet`, `profit`)
         VALUES ('$user_uid', 'total', '',
-            '', '0', '0', '$total_profit')
+            '', '0', '0', '$total_result')
     ");
 
-    db_query("DELETE FROM `minesweeper` WHERE `user_uid` = '$user_uid'");
+    db_query("
+        DELETE FROM `rolls`
+        WHERE `user_uid` = '$user_uid'
+            AND YEAR(`timestamp`) = '$year'
+            AND MONTH(`timestamp`) = '$month'
+    ");
     db_query("COMMIT");
 }
 
