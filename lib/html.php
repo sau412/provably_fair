2845d6f326108fafab1f4e5d2378940d9219ad8f
sau412 <sau@arikado.xyz>
2024-01-17 21:47:08 +0300

    Exchange limits 11

--- lib/html.php
+++ lib/html.php
@@ -1111,7 +1111,7 @@ function updateExchangeAmount() {
         let to_currency = getcurrencyDataByUid(to_currency_uid);
         
         let from_amount = parseFloat($("#from_amount").val());
-        let rate = to_currency.exchange_limit / (from_currency.exchange_limit + from_amount);
+        let rate = to_currency.exchange_limit / (from_currency.exchange_limit - from_amount);
         
         if(from_currency_uid == to_currency_uid) {
                 $("#exchange_message").text('You should not exchange currency to itself');
